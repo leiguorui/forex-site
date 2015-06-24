a1e29916073b5c52b387b986226ae11736f7c8c1
degree.lei@gmail.com <degree.lei@gmail.com>
2015-06-24 14:25:26 +0800

    first commit

---
+++ src/main/java/cn/injava/forex/web/WebController.java
@@ -0,0 +1,18 @@
+package cn.injava.forex.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class WebController {
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+        return "index";
+    }
+
+}
